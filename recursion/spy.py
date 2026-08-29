50c4893f1652c98d12dc3fdecaf3befa988efe5e
HarshAryan06 <aryan.harsh06@gmail.com>
2026-08-29 14:58:25 +0530

    spy no

--- recursion/spy.py
+++ recursion/spy.py
@@ -1,18 +1,33 @@
-def sum_digit(num):
+# def sum_digit(num):
+#     if num == 0:
+#         return 0
+#     return (num % 10) + sum_digit(num // 10)
+
+
+# def product_digit(num):
+#     if num == 0:
+#         return 1
+#     return (num % 10) * product_digit(num // 10)
+
+
+# num = 1124
+
+# if sum_digit(num) == product_digit(num):
+#     print("Spy Number")
+# else:
+#     print("Not a Spy Number")
+
+
+
+#------------------------using one function ---------------------------------------------------
+
+def spy(num,add,prod):
     if num == 0:
-        return 0
-    return (num % 10) + sum_digit(num // 10)
-
-
-def product_digit(num):
-    if num == 0:
-        return 1
-    return (num % 10) * product_digit(num // 10)
-
-
-num = 1124
-
-if sum_digit(num) == product_digit(num):
-    print("Spy Number")
-else:
-    print("Not a Spy Number")
+        if add == prod:
+            return "spy"
+        return "not a spy"
+    return spy(num // 10,add + (num % 10),prod * (num % 10))
+num = 123
+add= 0
+prod = 0
+print(spy(num,add,prod))
